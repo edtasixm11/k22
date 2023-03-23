88c922003c40917ff966124fe59cb10fb1e6744c
ecanet <ecanet@d09.informatica.escoladeltreball.org>
2023-03-23 09:19:52 +0100

    m11 kerberos

--- k22:kserver/install.sh
+++ k22:kserver/install.sh
@@ -8,7 +8,7 @@ cp /opt/docker/kadm5.acl /etc/krb5kdc/kadm5.acl
 
 kdb5_util create -s -P masterkey
 
-# Usuaris que s'utilitzaran amb LDAP de IP
+# Usuaris que s'utilitzaran amb LDAP de IP, Kerberos de AP
 for user in anna pere marta jordi pau user{01..10} 
 do
   kadmin.local -q "addprinc -pw k$user $user"
@@ -20,10 +20,19 @@ kadmin.local -q "addprinc -pw kpau  pau/admin"
 kadmin.local -q "addprinc -pw ksuper super"
 kadmin.local -q "addprinc -pw kadmin admin "
 
-# Usuaris que s'utilitzaran amb /etc/passwd de IP
+# Usuaris que s'utilitzaran amb /etc/passwd de IP i kerberos de AP
 for user in kuser{01..06}
 do
+  useradd -m -s /bin/bash $user
   kadmin.local -q "addprinc -pw $user $user"
 done
 
+# Usuaris purs unix, amb /etc/passwd IP i AP
+for user in unix01 unix02 unix03 unix04 unix05
+do
+  useradd -m -s /bin/bash $user
+  echo -e "$user\n$user" | passwd $user
+done
+
+# Generar el principal per al servidor SSH
 kadmin.local -q "addprinc -randkey host/sshd.edt.org"
