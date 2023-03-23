4e4476906e5651c748cbcff87f7f5e30c223532a
edtasixm11 <edtasixm11@gmail.com>
2023-03-23 09:34:18 +0100

    m11 kerberos

--- k22:kpam/install.sh
+++ k22:kpam/install.sh
@@ -3,4 +3,8 @@
 # @edt ASIX M11-SAD Curs 2022-2023
 
 cp /opt/docker/krb5.conf /etc/krb5.conf
+cp /opt/docker/common-auth /etc/pam.d/common-auth
+cp /opt/docker/common-account /etc/pam.d/common-account
+cp /opt/docker/common-session /etc/pam.d/common-session
+cp /opt/docker/common-password /etc/pam.d/common-password
 
