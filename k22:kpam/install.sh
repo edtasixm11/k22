4c9d734f67cb496b92a11cbf871e1215b97061ff
edtasixm11 <edtasixm11@gmail.com>
2023-03-23 09:43:24 +0100

    m11 kerberos

--- k22:kpam/install.sh
+++ k22:kpam/install.sh
@@ -8,3 +8,16 @@ cp /opt/docker/common-account /etc/pam.d/common-account
 cp /opt/docker/common-session /etc/pam.d/common-session
 cp /opt/docker/common-password /etc/pam.d/common-password
 
+# Usuaris purs unix, amb /etc/passwd IP i AP
+for user in unix01 unix02 unix03 unix04 unix05
+do
+  useradd -m -s /bin/bash $user
+  echo -e "$user\n$user" | passwd $user
+done
+
+# Usuaris  només per practicar fins que no es configuri LDAP de IP
+for user in anna pere marta jordi pau user{01..10} 
+do
+  useradd -m -s /bin/bash $user
+done
+
